b4638244fb5fdbe53b248e82202629dbc2a9f8c3
EnzoCordovana <enzo.cordovana@etu.univ-amu.fr>
2025-02-11 16:20:15 +0100

    creation de l'interface pour se deplacer

---
+++ src/yaip6/Moveable.java
@@ -0,0 +1,8 @@
+package yaip6;
+
+public interface Moveable {
+	abstract public void moveUp();
+	abstract public void moveDown();
+	abstract public void moveLeft();
+	abstract public void moveRight();
+}
